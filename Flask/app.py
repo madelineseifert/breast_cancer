67e2ba271d08a6076ccdaa6e753aa60fd0b15453
Blanca <b.pina.pinon@gmail.com>
2018-11-17 15:03:29 -0800

    Flask Update - take 4 inputs

--- Flask/app.py
+++ Flask/app.py
@@ -11,15 +11,19 @@ db = SQLAlchemy(app)
 
 class Patient(db.Model):
       id = db.Column(db.Integer, primary_key = True)
-      radius_mean = db.Column(db.Float)
-      texture_mean = db.Column(db.Float)
+      radius_worst = db.Column(db.Float)
+      concave_points_worst = db.Column(db.Float)
+      area_worst = db.Column(db.Float)
+      perimeter_worst = db.Column(db.Float)
 
-      def __init__(self,radius_mean,texture_mean):
-            self.radius_mean = radius_mean
-            self.texture_mean = texture_mean
+      def __init__(self,radius_worst,concave_points_worst,area_worst,perimeter_worst):
+            self.radius_worst = radius_worst
+            self.concave_points_worst = concave_points_worst
+            self.area_worst = area_worst
+            self.perimeter_worst = perimeter_worst                        
 
-      def __repr__(self):
-            return '<Test %r>' %  self.diagnosis
+      # def __repr__(self):
+      #       return '<Test %r>' %  self.diagnosis
 
 
 @app.route('/')
@@ -30,7 +34,10 @@ def index():
 
 @app.route('/post_patient_data',methods =['POST']) 
 def post_patient_data():
-      patient = Patient(request.form['radius_mean'],request.form['texture_mean'])   
+      patient = Patient(request.form['radius_worst'],
+                        request.form['concave_points_worst'],\
+                        request.form['area_worst'],\
+                        request.form['perimeter_worst'])   
       db.session.add(patient)
       db.session.commit() 
       return redirect(url_for('index'))
